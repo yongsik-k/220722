b9de14cc6e443f86686c3f0f5187b183afd9aa99
yongsik <kmjnhb777@naver.com>
2022-07-22 15:51:41 +0900

    220722

---
+++ 1.bootstrap.js
@@ -0,0 +1,9 @@
+const bootStrap1 = document.querySelector('.sample-btn1');
+const boootStrap2 = document.querySelector('.sample-btn2');
+bootStrap1.addEventListener('click', function(){
+alert("HI");
+})
+boootStrap2.addEventListener('click',function(){
+boootStrap2.classList.toggle("btn-sm")
+
+})
